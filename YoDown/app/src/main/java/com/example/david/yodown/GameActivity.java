5633944b376da67ea5f01de2fbc905292336ff08
David <davidCdong_123@hotmail.com>
2014-11-01 18:13:28 -0400

    maybe working google play location

--- YoDown/app/src/main/java/com/example/david/yodown/GameActivity.java
+++ YoDown/app/src/main/java/com/example/david/yodown/GameActivity.java
@@ -1,6 +1,12 @@
 package com.example.david.yodown;
 
+import android.app.Activity;
+import android.app.Dialog;
+import android.content.IntentSender;
+import android.location.Location;
+import android.support.v4.app.DialogFragment;
 import android.content.Intent;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,17 +16,59 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.location.LocationClient;
 
-public class GameActivity extends ActionBarActivity {
+public class GameActivity extends ActionBarActivity implements GooglePlayServicesClient.ConnectionCallbacks,
+                                                                                                GooglePlayServicesClient.OnConnectionFailedListener {
 
+    private LocationClient mLocationClient;
+    private Location mCurrentLocation;
     private ListView enemiesList;
 
     private String username = "";
     private ArrayList<String> enemies;
+
+    private static final int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        Toast.makeText(this, "Connected", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onDisconnected() {
+        Toast.makeText(this, "Disconnected", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        if(connectionResult.hasResolution()){
+            try{
+                connectionResult.startResolutionForResult(this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+            }catch(IntentSender.SendIntentException e){
+                e.printStackTrace();
+            }
+        }else{
+            showErrorDialog(connectionResult.getErrorCode());
+        }
+    }
+
+    public void showErrorDialog(int errorCode){
+        Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(errorCode, this, CONNECTION_FAILURE_RESOLUTION_REQUEST);
+        if(errorDialog != null){
+            ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+            errorFragment.setDialog(errorDialog);
+            errorFragment.show(getSupportFragmentManager(), "Location Updates");
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,8 +95,24 @@ public class GameActivity extends ActionBarActivity {
                 Log.v("Item", enemies.get(position));
             }
         });
+
+        mLocationClient = new LocationClient(this, this, this);
+
+        //mCurrentLocation = mLocationClient.getLastLocation();
+        //Toast.makeText(this, Double.toString(mCurrentLocation.getLatitude())+Double.toString(mCurrentLocation.getLongitude()), Toast.LENGTH_SHORT).show();
     }
 
+    @Override
+    public void onStart(){
+        super.onStart();
+        mLocationClient.connect();
+    }
+
+    @Override
+    public void onStop(){
+        mLocationClient.disconnect();
+        super.onStop();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -71,4 +135,42 @@ public class GameActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public static class ErrorDialogFragment extends DialogFragment{
+        private Dialog mDialog;
+        public ErrorDialogFragment(){
+            super();
+            mDialog = null;
+        }
+        public void setDialog(Dialog dialog){
+            mDialog = dialog;
+        }
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState){
+            return mDialog;
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        switch(requestCode){
+            case CONNECTION_FAILURE_RESOLUTION_REQUEST  :
+                switch (resultCode){
+                    case Activity.RESULT_OK:
+                        //TODO:try request again
+                    break;
+                }
+        }
+    }
+
+    private boolean servicesConnected(){
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if(ConnectionResult.SUCCESS == resultCode){
+            Log.d("Location updates", "Google play service is available");
+            return true;
+        }else{
+            showErrorDialog(resultCode);
+        }
+        return false;
+    }
+
 }
