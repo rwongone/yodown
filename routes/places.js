6efe5a95705b5af6ca008ac05f9cf3e4aa78577c
Kevin <ivexnuke@gmail.com>
2014-11-02 06:38:25 -0500

    ok

--- routes/places.js
+++ routes/places.js
@@ -47,7 +47,7 @@ router.post('/', function(req, res) {
 					User.find({
 						//'location.latitude': { $gte: latlong.latitude-range, $lte: latlong.latitude+range },
 						//'location.longitude': { $gte: latlong.longitude-range, $lte: latlong.longitude+range },
-						//user_id: {$ne: user_id},
+						user_id: {$ne: user_id},
 						'dead':  false
 						}, function (err, users) {
 							console.log("users:");
