65bf9b7ecc89d8131dbadb9c0a36dfc2ebde2f07
Kevin <ivexnuke@gmail.com>
2014-11-02 06:40:41 -0500

    ok

--- routes/places.js
+++ routes/places.js
@@ -42,7 +42,7 @@ router.post('/', function(req, res) {
 						}
 					});
 
-					var range = 1000;
+					var range = 5;
 					console.log(latlong.latitude-range);
 					console.log("^range");
 					/*********************Start Fetching Nearby Users**********************/
